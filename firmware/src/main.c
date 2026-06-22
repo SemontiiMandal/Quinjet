a0648f4b310a85da352367f89b752efa39ab81b4
SemontiiMandal <semontiimandal2006@gmail.com>
2026-06-21 22:11:43 -0400

    Update drone-journal, overlay and sensor fusion code

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -3,19 +3,13 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/sys/printk.h>
 
-int main(void) {
-    const struct device *const bmi270 = DEVICE_DT_GET(DT_NODELABEL(bmi270));
-    struct sensor_value accel[3];
-    struct sensor_value gyro[3];
+static void bmi270_trigger_handler(const struct device *dev, struct sensor_trigger *trig){
+    struct sensor_value val;
 
-    if (!device_is_ready(bmi270)) {
-        printk("Device BMI270 is not ready.\n");
-        return 0;
-    }
+    if (trig->type == SENSOR_TRIG_DATA_READY){
 
-    printk("BMI270 found, fetching data...\n");
+         printk("BMI270 data arrived, fetching now...\n");
 
-    while (1) {
         // Fetch sensor samples 
         sensor_sample_fetch(bmi270);
 
@@ -25,9 +19,33 @@ int main(void) {
         // Get Gyroscope Data
         sensor_channel_get(bmi270, SENSOR_CHAN_GYRO_XYZ, gyro);
 
-        printk("Accel X: %d.%06d m/s^2\n", accel[0].val1, accel[0].val2);
-        printk("Gyro X: %d.%06d rad/s\n", gyro[0].val1, gyro[0].val2);
+        printk("Accel X: %d.%06d m/s^2\n", accel.val1, accel.val2);
+        printk("Gyro X: %d.%06d rad/s\n", gyro.val1, gyro.val2);
+    }
 
-        k_sleep(K_MSEC(100));
+}
+
+static void bmi270_init(){
+    const struct device *const bmi270 = DEVICE_DT_GET(DT_NODELABEL(bmi270));
+     struct sensor_trigger trig;
+
+    if (!device_is_ready(bmi270)) {
+        printk("Device BMI270 is not ready.\n");
+        return 0;
+    }
+
+    if (!bmi270_init_interrupts(bmi270)){
+        printk("Interrupt Trigger Enabled for BMI270. \n")
+    }   
+
+    trig.type = SENSOR_TRIG_DATA_READY;
+    trig.chan = SENSOR_CHAN_ALL;
+
+     if (!bmi270_trigger_set(dev, &trig, bmi270_trigger_handler)){
+         printf("Trigger set successfully!\n");
+     }
+
+    while (1) {
+      k_sleep_ms(K_FOREVER);
     }
 }
